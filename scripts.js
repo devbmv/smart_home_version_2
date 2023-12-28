525a120252d5dd6cc2196e47a37cb12a6d623abd
devbmv <maik_775@yahoo.com>
2023-12-28 21:19:17 +0300

    Update Readme

--- scripts.js
+++ scripts.js
@@ -1,20 +1,15 @@
 function handleVisibilityChange() {
     if (document.visibilityState === 'hidden') {
-        // Codul pe care dorești să-l execuți la ascunderea paginii
-        console.log('Pagina este ascunsă.');
+        console.log('The page is hidden');
     } else {
-        // Codul pe care dorești să-l execuți când pagina devine vizibilă din nou
-        console.log('Pagina este vizibilă.');
+        console.log('Page is visible.');
     }
 }
 
 function handlePageHide(event) {
-    // Codul pe care dorești să-l execuți atunci când utilizatorul navighează departe de pagină
-    console.log('Utilizatorul navighează departe de pagină.');
+    console.log('User is out of page.');
 }
 
-// Adaugă event listener pentru evenimentul visibilitychange
 document.addEventListener('visibilitychange', handleVisibilityChange);
 
-// Adaugă event listener pentru evenimentul pagehide
 window.addEventListener('pagehide', handlePageHide);
